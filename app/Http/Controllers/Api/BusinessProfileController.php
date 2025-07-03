53f603b033cb231517096923da83b05dbfbaeb2d
julisavani <julisavani44@gmail.com>
2025-07-03 15:50:38 +0530

    first commit

---
+++ app/Http/Controllers/Api/BusinessProfileController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\BusinessProfile;
+use App\Models\UserKyc;
+use Illuminate\Http\Request;
+
+class BusinessProfileController extends Controller
+{
+     
+    public function index(){
+        $profile = BusinessProfile::where('user_id', auth()->id())->first();
+
+        if (!$profile) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Business profile not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data' => $profile,
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $commonRules = [
+            'business_type' => 'required|in:individual,business,brand,company',
+            'address' => 'required|string',
+            'name' => 'required|string',
+        ];
+
+        $typeSpecificRules = match ($request->input('business_type')) {
+            'business' => [
+                'gst' => 'required|string|regex:/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/',
+            ],
+            'brand' => [
+                'cin' => 'required|string|regex:/^([A-Z]{1}[0-9]{5}[A-Z]{2}[0-9]{4}[A-Z]{3}[0-9]{6})$/',
+            ],
+            'company' => [
+                'pan' => 'required|string|regex:/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/',
+            ],
+        };
+
+        $validator = Validator::make($request->all(), array_merge($commonRules, $typeSpecificRules));
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        $profile = BusinessProfile::create([
+            'user_id' => Auth::id(), // Or pass in $request->user_id if admin submits
+            'business_type' => $request->business_type,
+            'name' => $request->name,
+            'gst' => $request->gst,
+            'cin' => $request->cin,
+            'pan' => $request->pan,
+            'address' => $request->address,
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Business profile submitted successfully',
+        ]);
+    }
+}
---
+++ database/migrations/2024_05_14_110640_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('service_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('influencer_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('gateway_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('status')->default('awaiting'); // awaiting, active, complete, canceled
+            $table->string('payment_status')->default('unpaid'); // paid, unpaid, rejected
+            $table->double('amount')->nullable();
+            $table->double('charge')->default(0);
+            $table->double('discount_amount')->default(0);
+            $table->double('total_amount'); // This is grand total amount including, charge, discount and amount
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->string('file')->nullable();
+            $table->string('reason')->nullable();
+            $table->text('description')->nullable();
+            $table->longText('meta')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
