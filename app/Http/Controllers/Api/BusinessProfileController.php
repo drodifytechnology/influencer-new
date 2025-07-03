b3bdbc60f1c3e6db2080f78a95f5a702d44778ea
julisavani <julisavani44@gmail.com>
2025-07-03 16:32:11 +0530

    first commit

--- app/Http/Controllers/Api/BusinessProfileController.php
+++ app/Http/Controllers/Api/BusinessProfileController.php
@@ -33,7 +33,7 @@ class BusinessProfileController extends Controller
             'name' => 'required|string',
         ];
 
-        $typeSpecificRules = match ($request->input('business_type')) {
+       $typeSpecificRules = match ($request->input('business_type')) {
             'business' => [
                 'gst' => 'required|string|regex:/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/',
             ],
@@ -43,6 +43,7 @@ class BusinessProfileController extends Controller
             'company' => [
                 'pan' => 'required|string|regex:/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/',
             ],
+            default => [] // Handles 'individual' or any fallback case
         };
 
         $validator = Validator::make($request->all(), array_merge($commonRules, $typeSpecificRules));
