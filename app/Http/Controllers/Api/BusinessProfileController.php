1b205c5ba224f9a8d8283f87f3783e36da827c02
julisavani <julisavani44@gmail.com>
2025-07-03 16:40:55 +0530

    first commit

--- app/Http/Controllers/Api/BusinessProfileController.php
+++ app/Http/Controllers/Api/BusinessProfileController.php
@@ -41,11 +41,9 @@ class BusinessProfileController extends Controller
                 'gst' => 'required|string|regex:/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/',
             ],
             'brand' => [
-                'company_name' => 'required|string|max:100',
                 'cin' => 'required|string|regex:/^([A-Z]{1}[0-9]{5}[A-Z]{2}[0-9]{4}[A-Z]{3}[0-9]{6})$/',
             ],
             'company' => [
-                'company_name' => 'required|string|max:100',
                 'pan' => 'required|string|regex:/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/',
             ],
             default => [], // 'individual' needs no extra rules
